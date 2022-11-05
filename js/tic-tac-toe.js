c4aaab983a7d896f0e9b801fa66d50213295e71f
Chad Chapman <chad.chapman2010@gmail.com>
2022-11-05 19:26:05 -0400

    Added some functions to get thing started and to test initialization.  Set effects when start game button is clicked.

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -5,8 +5,49 @@
  *  Description: Functions that support implementation of Tic Tac Toe game.
 ******************************************************************************/
 
-function game() {
-    let gameBoard = [["", "", ""] ,
-                    ["", "", ""],
-                    ["", "", ""]];
+
+    
+let gameInstance = {
+    gameBoard: [["-", "-", "-"],
+                ["-", "-", "-"],
+                ["-", "-", "-"]],
+                
+    init: function() {
+        this.cacheDOM();
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        this.$button.on('click', this.setInitialGameState.bind(this));
+    },
+    cacheDOM: function () {
+        this.$el = $("#game");
+        this.$button = this.$el.find("#start-game");
+        this.gameButtonContainer = this.$el.find(".game-button-container");
+    },
+
+    
+    setInitialGameState: function() {
+        for(let i = 0; i < this.gameBoard.length; i++) {
+            for(let j = 0; j < this.gameBoard[i].length; j++) {
+                $(`#row${i}-col${j}`).text(`${i}${j}`);
+                console.log(`${i}${j}`);
+                console.log(this.gameBoard[i][j]);
+            }
+        }
+
+        // Add effects
+        $(this.gameButtonContainer).effect( "bounce", {times:3}, 300 );
+        $(this.$button).prop("disabled", true);
+    },
+
+};
+
+gameInstance.init();
+
+
+
+const player = (symbol) => {
+    
 }
+
